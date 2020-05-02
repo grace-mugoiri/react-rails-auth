aa6fd0d15e3af645a3169022a60a65f95e39c312
grace-mugoiri <gracemugoiri@gmail.com>
2020-05-02 19:09:21 +0300

    restructure tree

---
+++ react-rails-auth/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Registrations from './auth/Registrations'
+
+export default class extends Component{
+	render() {
+		return (
+			<div>
+				<h2>Home</h2>
+				<h2>Home two</h2>
+				<Registrations />
+			</div>
+		)
+	}
+}
