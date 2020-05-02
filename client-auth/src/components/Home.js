e14be0580aa64d5cd6cb17b2c0479e5859a3e86d
grace-mugoiri <gracemugoiri@gmail.com>
2020-05-02 21:31:23 +0300

    redirect user to dashboard after succes reg

--- client-auth/src/components/Home.js
+++ client-auth/src/components/Home.js
@@ -4,13 +4,18 @@ import Registrations from './auth/Registrations'
 export default class extends Component{
 	constructor(props) {
 		super(props);
+		this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+	}
+
+	handleSuccessfulAuth(data) {
+		this.props.history.push('/dashboard');
 	}
 	render() {
 		return (
 			<div>
 				<h2>Home</h2>
 				<h2>Status: {this.props.loggedInStatus}</h2>
-				<Registrations />
+				<Registrations handleSuccessfulAuth={this.handleSuccessfulAuth} />
 			</div>
 		)
 	}
--- client-auth/src/components/auth/Registrations.js
+++ client-auth/src/components/auth/Registrations.js
@@ -36,9 +36,10 @@ export default class Registration extends Component {
 		},
 		{ withCredentials: true }
 		)
-			.then(
-			response => {
-				console.log("regirstration res", response);
+			.then(response => {
+				if (response.data.status === 'created') {
+					this.props.handleSuccessfulAuth(response.data);
+				}
 			})
 			.catch(error => {
 				console.log("reg error", error);
